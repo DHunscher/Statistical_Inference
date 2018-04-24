71ca49e88aee57194fbbf9a55ae186b93d50b1f1
DALE HUNSCHER <dalehunscher@DalesMacBookAir.attlocal.net>
2018-04-24 17:08:30 -0400

    Project files completed, first draft.

---
+++ project/test1.R
@@ -0,0 +1,17 @@
+
+setwd("/Users/dalehunscher/Dropbox/Coursera/Statistical_Inference/project")
+
+````
+library(knitr) 
+library(ggplot2) 
+library(graphics)
+library(datasets)
+
+# load the data set into a transient variable
+
+tooth <- ToothGrowth
+```
+
+```
+summary(tooth)
+```
